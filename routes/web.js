d74818c4df012c7958b238c9fc4a4f5bb17f090e
ShreyanshMehta12 <Shreyanshmehta55@gmail.com>
2023-12-06 21:33:56 +0530

    kk

--- routes/web.js
+++ routes/web.js
@@ -22,7 +22,7 @@ router.get('/logout',UserController.logout)
 router.get('/profile',UserController.profile)
 router.post('/updatepassword',CheckUserAuth,UserController.changepassword)
 router.post('/updateprofile',CheckUserAuth,UserController.updateprofile)
-router.get('/me',UserController.get_user_detail)
+router.get('/me',CheckUserAuth,UserController.get_user_detail)
 router.get('/getalluser',UserController.get_all_user)
 
 //category controller
