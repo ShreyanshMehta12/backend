316e039fa9311b8b70f9fb32e6cdac156e42fb4f
ShreyanshMehta12 <Shreyanshmehta55@gmail.com>
2023-12-15 22:45:13 +0530

    okk

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,10 +12,10 @@ const CheckUserAuth=async(req,res,next)=>{
     }
     else{
         const verify_token=jwt.verify(token,'kishanmehta12')
-        const data=await UserModal.findOne({_id:verify_token.id})
+        const data=await UserModal.findById({_id:verify_token.id})
         //console.log(data)
         req.user=data
-        console.log(verify_token)
+        // console.log(verify_token)
         next()
     }
 }
