e496b825b5124dbd4a18634f655fe657bb5714b5
shreyansh12 <shreyanshmehta55@gmail.com>
2024-01-21 20:54:42 +0530

    kkk

--- models/User.js
+++ models/User.js
@@ -2,39 +2,51 @@ const mongoose = require('mongoose')
 
 // define schema
 const UserSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,    
-    },
-    email:{
-        type:String,
-        required:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-    // cpassword:{
-    //   type:String,
-    //   required:true,
-    // },
-    image:
-    {
-      public_id: {
-        type: String,
-        
-      },
-      url: {
-        type: String,
-         
-      },
-    },
-    role:{
-      type: String,
-      default: 'student'
-    }
     
-},{timestamps:true})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  number: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+  state: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  pincode: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  image: {
+    public_id: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
+  },
+  role: {
+    type: String,
+    default: "student",
+  },
+},{ timestamps: true }
+);
 
 // create collection
 
